96707640e95a0d06b5c221b68b5649694d86776d
Abuzar Gaur <your-email@gmail.com>
2026-03-04 16:15:40 +0530

    multiple -route-handler

--- src/app.js
+++ src/app.js
@@ -1,35 +1,41 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-// this will only handle Get call to / user
+// app.use("/route",rH, [ rH2,rH3] ,rH4 ,rH5)
 
-// app.use("/user", (req , res)=>{
-//     res.send("HAHAHAHA");
-// })
+app.use(
+  "/user",
+  [
+    (req, res, next) => {
+    // Route Handler
+    //  res.send("Route Handler 1")
+    console.log("Handling the routes user;");
+    // res.send("Response!!");
+    next();
+  },
+  (req, res,next) => {
+    console.log("Handling the routes user 2;");
+    // res.send("Response! 2");
+    next()
+  },
+  (req, res,next) => {
+    console.log("Handling the routes user 3;");
+    // res.send("Response! 3");
+    next()
+  },
+  (req, res,next) => {
+    console.log("Handling the routes user 4;");
+    // res.send("Response! 4");
+    next()
+  },
+  (req, res,next) => {
+    console.log("Handling the routes user 5;");
+    res.send("Response! 5");
+  }
+  ]
+);
 
-// app.get("/user", (req , res)=>{
-//     console.log(req.query)
-//     res.send({firstName :"abuzar" , "lastname":"gaur"});
-// })
-
-app.get("/user/:userId/:name", (req , res)=>{
-    console.log(req.params)
-    res.send({firstName :"abuzar" , "lastname":"gaur"});
-})
-
-app.post("/user", (req , res)=>{
-    res.send("data sucessfully save to database");
-})
-app.delete("/user", (req , res)=>{
-    res.send("delete sucessfully");
-})
-//this will match all the HTTP method API calls to /test 
-
-app.use("/test",(req ,res)=>{
-    res.send("hello from the server");
-})
-
-app.listen(3000,()=>{
-    console.log('server is sucessfully listeing on port 3000');
+app.listen(3000, () => {
+  console.log("server is sucessfully listeing on port 3000");
 });
---
+++ src/copy2.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const app = express();
+
+// this will only handle Get call to / user
+
+// app.use("/user", (req , res)=>{
+//     res.send("HAHAHAHA");
+// })
+
+// app.get("/user", (req , res)=>{
+//     console.log(req.query)
+//     res.send({firstName :"abuzar" , "lastname":"gaur"});
+// })
+
+app.get("/user/:userId/:name", (req , res)=>{
+    console.log(req.params)
+    res.send({firstName :"abuzar" , "lastname":"gaur"});
+})
+
+app.post("/user", (req , res)=>{
+    res.send("data sucessfully save to database");
+})
+app.delete("/user", (req , res)=>{
+    res.send("delete sucessfully");
+})
+//this will match all the HTTP method API calls to /test 
+
+app.use("/test",(req ,res)=>{
+    res.send("hello from the server");
+})
+
+app.listen(3000,()=>{
+    console.log('server is sucessfully listeing on port 3000');
+});
